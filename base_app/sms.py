9bc8b914836344134f33d10051e402715be9b7eb
Sanket Chouriya <109847409+Sanket7994@users.noreply.github.com>
2023-06-07 16:14:18 +0530

    commit2

--- base_app/sms.py
+++ base_app/sms.py
@@ -1,21 +1,21 @@
 # Download the helper library from https://www.twilio.com/docs/python/install
 from school import settings
 from twilio.rest import Client
+
 # Set environment variables for your credentials
 # Read more at http://twil.io/secure
 
 
-def send_sms_notification(patient, staff_member):
+def send_sms_notification_patient(patient, staff_member):
     relatedRecipient_firstName = staff_member.get("first_name")
     relatedRecipient_lastName = staff_member.get("last_name")
-    patientNumber = ''.join(list(patient.get("contact_number")))
-    staff_memberNumber = ''.join(list(staff_member.get("contact_number")))
-    
+    patientNumber = "".join(list(patient.get("contact_number")))
+  
     account_sid = settings.TWILIO_ACCOUNT_SID
     auth_token = settings.TWILIO_AUTH_TOKEN
     client = Client(account_sid, auth_token)
     message = client.messages.create(
-    body=f"""
+        body=f"""
 Hi {patient.get('patient_first_name')}, 
 
 An appointment has been successfully setup with Dr,{relatedRecipient_firstName} {relatedRecipient_lastName} on {patient.get('appointment_date')} at assigned time slot of {patient.get('appointment_slot')}. 
@@ -25,10 +25,22 @@ Our Team will be waiting for your arrival. Thanks for choosing us.
 Kind regards,
 project Support Team
 """,
-    from_="+13614597582",
-    to=patientNumber,
-    
-    body=f"""
+        from_="+13614597582",
+        to=patientNumber,
+    )
+    print(message.sid)
+
+
+def send_sms_notification_staff_member(staff_member):
+    relatedRecipient_firstName = staff_member.get("first_name")
+    relatedRecipient_lastName = staff_member.get("last_name")
+    staff_memberNumber = "".join(list(staff_member.get("contact_number")))
+
+    account_sid = settings.TWILIO_ACCOUNT_SID
+    auth_token = settings.TWILIO_AUTH_TOKEN
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body=f"""
 Hi {staff_member.get('first_name')}, 
 
 An appointment has been successfully setup with the client. 
@@ -36,13 +48,8 @@ please check the dashboard for more information.
 
 Kind regards,
 project Support Team
-"""
-)
-#print(message.sid)
-
-
-
-
-
-
-
+""",
+        from_="+13614597582",
+        to=staff_memberNumber,
+    )
+    print(message.sid)
